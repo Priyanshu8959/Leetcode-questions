a9f23b5566fc4351c075e96f49c028548ef28806
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2025-10-03 05:12:06 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70.76%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,28 +1,26 @@
 class Solution {
-private:
+public:
     static constexpr int mod = 1000000007;
 
-    // fast exponentiation helper
-    long long power(long long base, long long exp) {
+    long long power(long long x, long long p) {
         long long result = 1;
-        while (exp > 0) {
-            if (exp % 2 == 1) {
-                result = (result * base) % mod;
+        while (p > 0) {
+            if (p % 2 == 1) {
+                result = (result * x) % mod;
             }
-            base = (base * base) % mod;
-            exp /= 2;
+            x = (x * x) % mod;
+            p /= 2;
         }
         return result;
     }
 
-public:
     int countGoodNumbers(long long n) {
-        long long evenCount = (n + 1) / 2;
-        long long oddCount = n / 2;
+        long long evensteps = (n + 1) / 2;
+        long long oddsteps = n / 2;
 
-        long long part1 = power(5, evenCount);
-        long long part2 = power(4, oddCount);
+        long long temp1 = power(5, evensteps);
+        long long temp2 = power(4, oddsteps);
 
-        return (part1 * part2) % mod;
+        return (temp1 * temp2) % mod;
     }
 };
