4d23622da8120e1597f402cc07b37b5d4d82b226
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2025-10-11 05:02:32 +0530

    Time: 11 ms (85.82%), Space: 85.1 MB (63.26%) - LeetHub

---
+++ 1814-jump-game-vi/1814-jump-game-vi.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> t(n);
+        t[0] = nums[0];
+        dq.push_back(0);
+
+        for (int i = 1; i < n; ++i) {
+            while (!dq.empty() && dq.front() < i - k)
+                dq.pop_front();
+
+            t[i] = nums[i] + t[dq.front()];
+
+            while (!dq.empty() && t[dq.back()] <= t[i])
+                dq.pop_back();
+
+            dq.push_back(i);
+        }
+
+        return t[n - 1];
+    }
+};
