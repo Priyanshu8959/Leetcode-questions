42b9a22c0e2d6804b38e680c26443d4ce16a525f
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2025-10-21 07:01:03 +0530

    Time: 0 ms (100%), Space: 14 MB (90.8%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n = candidates.size();
-
-         vector<vector<vector<vector<int>>>> t(n + 1, vector<vector<vector<int>>>(target + 1));
-
-        // Base cases
-        for (int i = 0; i <= n; i++) {
-            t[i][0] = { { } };   // ek empty combination ban sakta hai sum 0 ke liye
-        }
-        for (int j = 1; j <= target; j++) {
-            t[0][j] = { };       // 0 candidates se positive sum nahi ban sakta
-        }
-
-        // Fill DP table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                // Exclude current candidate
-                t[i][j] = t[i-1][j];
-
-                // Include current candidate (unlimited usage)
-                if (candidates[i-1] <= j) {
-                    for (auto comb : t[i][j - candidates[i-1]]) {
-                        comb.push_back(candidates[i-1]);
-                        t[i][j].push_back(comb);
-                    }
-                }
+    void fn(vector<vector<int>>& answer, vector<int>& ds,
+            vector<int>& candidates, int target, int index) {
+        // base condition
+        if (index == candidates.size()) {
+            if (target == 0) {
+                answer.push_back(ds);
             }
+            return;
         }
 
-        return t[n][target];
+        // pick and not pick condition
+        if (candidates[index] <= target) {
+            ds.push_back(candidates[index]);
+            fn(answer, ds, candidates, target - candidates[index], index);
+            ds.pop_back();
+        }
+        fn(answer, ds, candidates, target, index + 1);
+    }
+
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> answer;
+        vector<int> ds;
+        fn(answer, ds, candidates, target, 0);
+        return answer;
     }
 };
