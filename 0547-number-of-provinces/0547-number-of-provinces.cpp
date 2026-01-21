6e6a73927cfe92e71f67e4d29b9db7d14f5c7407
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2026-01-21 12:58:59 +0530

    Time: 3 ms (39.92%), Space: 20.6 MB (32.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& isConnected, vector<bool>& visited, int node) {
-     int n =     isConnected.size();
+    void bfs(vector<vector<int>>& adj, vector<bool>& visited, int node) {
+        queue<int> q;
         visited[node] = true;
+        q.push(node);
 
-        for (int neighbours = 0; neighbours < n; neighbours++) {
-            if (!visited[neighbours] && isConnected[node][neighbours] == 1) {
-                dfs(isConnected, visited, neighbours);
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+
+            for (auto &neighbour : adj[current]) {
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }
             }
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        int count = 0; // for counting number of provience
+        vector<vector<int>> adj(n);
+
+        // Correct matrix → list conversion
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        int count = 0;
         vector<bool> visited(n, false);
-        for (int vertax = 0; vertax < n; vertax++) {
-            if (!visited[vertax]) {
+
+        for (int v = 0; v < n; v++) {
+            if (!visited[v]) {
                 count++;
-                dfs(isConnected, visited, vertax );
+                bfs(adj, visited, v);
             }
         }
         return count;
