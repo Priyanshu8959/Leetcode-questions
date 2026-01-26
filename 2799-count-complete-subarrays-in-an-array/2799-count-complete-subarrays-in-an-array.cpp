9ed855eb5784c65e707cf0d4d9a65a55969d3133
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2026-01-26 10:45:09 +0530

    Time: 34 ms (32.81%), Space: 41.6 MB (87.36%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        unordered_map<int,int>mp1;
-         unordered_map<int,int>mp2;
-         for(int i= 0;i<nums.size();i++){
+        int n = nums.size();
+
+        unordered_map<int,int> mp1;
+        unordered_map<int,int> mp2;
+
+        for (int i = 0; i < n; i++) {
             mp1[nums[i]]++;
-         }
-         int n = mp1.size();
-        int count = 0;
-        for(int i=0;i<nums.size();i++){
-            for(int j = i;j<nums.size();j++){
-                    mp2[nums[j]]++;
-                    if(mp2.size()==n){
-                        count++;
-                    }
-            }
-            mp2.clear();
         }
-          
-         return count;
+
+        int x = mp1.size();   // total distinct elements
+
+        int i = 0, j = 0;
+        long long count = 0;
+
+        while (j < n) {
+            mp2[nums[j]]++;
+
+            // when window becomes complete
+            while (mp2.size() == x) {
+                count += (n - j);   // count all valid subarrays
+                mp2[nums[i]]--;
+                if (mp2[nums[i]] == 0) {
+                    mp2.erase(nums[i]);
+                }
+                i++;   // shrink from left
+            }
+
+            j++;   // expand window
+        }
+
+        return count;
     }
 };
