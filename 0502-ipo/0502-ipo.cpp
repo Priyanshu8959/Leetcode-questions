a059411387a67daa5c4502963a567ae4a07856f4
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2026-03-19 09:16:52 +0530

    Time: 72 ms (68.05%), Space: 131.4 MB (50.69%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        
+        vector<pair<int,int>> arr;
+        for(int i = 0; i < capital.size(); i++){
+            arr.push_back({capital[i], profits[i]});
+        }
+
+        sort(arr.begin(), arr.end());
+        priority_queue<int> pq;
+
+        int i = 0;
+        int n = arr.size();
+
+        while(k--){
+             while(i < n && arr[i].first <= w){
+                pq.push(arr[i].second);
+                i++;
+            }
+
+             if(pq.empty()){
+                break;
+            }
+
+             w += pq.top();
+            pq.pop();
+        }
+
+        return w;
+    }
+};
