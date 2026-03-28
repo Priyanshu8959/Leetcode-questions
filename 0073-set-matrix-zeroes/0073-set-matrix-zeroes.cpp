71d34bebb9af5fb6b73e5d6222cbe4cec02e9ff3
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2026-03-28 17:54:45 +0530

    Time: 0 ms (100%), Space: 20.9 MB (17.35%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        bool firstRowZero = false, firstColZero = false;
+
+        // Step 1: Check if first row has 0
+        for(int j = 0; j < m; j++){
+            if(matrix[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Step 2: Check if first column has 0
+        for(int i = 0; i < n; i++){
+            if(matrix[i][0] == 0){
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Step 3: Use first row & col as markers
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Step 4: Fill matrix based on markers
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 5: Handle first row
+        if(firstRowZero){
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 6: Handle first column
+        if(firstColZero){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
