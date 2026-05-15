f1d194aa64beebc3b497cf82d8c014f40a1317a9
Priyanshu Pawar <pawarpriyanshu198@gmail.com>
2026-05-15 18:15:59 +0530

    Time: 0 ms (100%), Space: 9.1 MB (23.59%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    void fn(vector<vector<int>>& answer, vector<int>& ds,
-            int k, int target, int value) {
-        // base conditions
-        if (target == 0 && ds.size() == k) {
-            answer.push_back(ds);
-            return;
-        }
-        if (target < 0 || value > 9 || ds.size() > k)
-            return;
-
-        // pick
-        ds.push_back(value);
-        fn(answer, ds, k, target - value, value + 1);
-        ds.pop_back();
-
-        // not pick
-        fn(answer, ds, k, target, value + 1);
+    // essensial things is 1) arr (1 to 9)
+    //                     2) k ;
+    //                     3) sum which is n
+    //                     4) ans vector of string for storing result 
+    //                     5) string s for current stirng of nubmers  
+    void helper(int k, int sum, vector<vector<int>>&ans,vector<int>&arr,int i,vector<int>&s){
+       int n = arr.size();
+        
+        
+       if(sum==0 && k==0){
+        ans.push_back(s);
+        return;
+       } 
+       if(k<0 || sum<0 || i==n){
+        return ;
+       }
+       helper(k,sum,ans,arr,i+1,s); //not pick
+       s.push_back(arr[i]);
+       helper(k-1,sum-arr[i],ans,arr,i+1,s); //pick
+       s.pop_back();
+        
     }
 
+
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> answer;
-        vector<int> ds;
-        fn(answer, ds, k, n, 1);
-        return answer;
+        vector<vector<int>>ans;
+        vector<int>s;
+        vector<int>arr={1,2,3,4,5,6,7,8,9};
+        helper(k,n,ans,arr,0,s);
+        return ans;
     }
 };
